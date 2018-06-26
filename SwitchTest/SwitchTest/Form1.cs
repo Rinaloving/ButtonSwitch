6bc8277b2536281097eb513ef38f6cf32f87ae7a
cfl <1243455143@qq.com>
2018-06-26 13:38:32 +0800

    Winform实现开关效果

---
+++ SwitchTest/SwitchTest/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SwitchTest
+{
+    public partial class Form1 : Form
+    {
+
+        public static bool state = true;
+        public Form1()
+        {
+            InitializeComponent();
+            CheckForIllegalCrossThreadCalls = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Thread tread = new Thread(TimeShow);
+            tread.Start();
+        }
+
+        public void TimeShow()
+        {
+            int j = 0;
+            int k = 0;
+                for (int i = 0; i < 100; i++)
+                {
+                    if (state)
+                    {
+                        Thread.Sleep(1000);
+                        label1.Text = (i-k).ToString();
+                        j++;
+                        this.Invalidate();
+                    }
+                    else
+                    {
+                       
+                        Thread.Sleep(1000);
+                        label2.Text = (i-j).ToString();
+                        k++;
+                        this.Invalidate();
+                    }
+                    
+                }
+            
+        }
+    }
+}
---
+++ SwitchTest/SwitchTest/UserControl1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SwitchTest
+{
+    public enum CheckStyle { style1 = 0 };
+    public partial class UserControl1 : UserControl
+    {
+        public UserControl1()
+        {
+            InitializeComponent();
+            //设置Style支持透明背景色并且双缓冲
+            this.SetStyle(ControlStyles.AllPaintingInWmPaint, true);
+            this.SetStyle(ControlStyles.DoubleBuffer, true);
+            this.SetStyle(ControlStyles.ResizeRedraw, true);
+            this.SetStyle(ControlStyles.Selectable, true);
+            this.SetStyle(ControlStyles.SupportsTransparentBackColor, true);
+            this.SetStyle(ControlStyles.UserPaint, true);
+            this.BackColor = Color.Transparent;
+
+            this.Cursor = Cursors.Hand;
+            this.Size = new Size(87, 27);
+        }
+
+
+        bool isCheck = false;
+        /// <summary>
+        /// 是否选中
+        /// </summary>
+        public bool Checked
+        {
+            set { isCheck = value; this.Invalidate(); }
+            get { return isCheck; }
+        }
+
+        CheckStyle checkStyle = CheckStyle.style1;
+        /// <summary>
+        /// 样式
+        /// </summary>
+        public CheckStyle CheckStyleX
+        {
+            set { checkStyle = value; this.Invalidate(); }
+            get { return checkStyle; }
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            Bitmap bitMapOn = null;
+            Bitmap bitMapOff = null;
+
+
+            if (checkStyle == CheckStyle.style1)
+            {
+                bitMapOn = global::SwitchTest.Properties.Resources.btncheckon1;
+                bitMapOff = global::SwitchTest.Properties.Resources.btncheckoff1;
+            }
+
+
+            Graphics g = e.Graphics;
+            Rectangle rec = new Rectangle(0,0,Size.Width, this.Size.Height);
+
+            if (isCheck)
+            {
+                g.DrawImage(bitMapOn,rec);
+                //MessageBox.Show("开关打开了！");
+                Form1.state = true;
+            }
+            else
+            {
+                g.DrawImage(bitMapOff,rec);
+                Form1.state = false;
+               // MessageBox.Show("开关关闭了！");
+            }
+        }
+        /// <summary>
+        /// 鼠标点击触发开关
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        //private void UserControl1_Click(object sender, EventArgs e)
+        //{
+        //    isCheck = !isCheck;
+        //    this.Invalidate(); //刷新
+        //}
+        private void UserControl1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void UserControl1_MouseClick(object sender, MouseEventArgs e)
+        {
+            isCheck = !isCheck;
+            this.Invalidate(); //刷新
+        }
+    }
+}
